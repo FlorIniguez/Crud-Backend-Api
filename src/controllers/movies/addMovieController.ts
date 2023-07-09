e8f02b739b0e6e851e0c39150f8c33e8ac50246f
FlorIniguez <iniguezflorencia788@gmail.com>
2023-07-08 22:53:42 -0300

    fin?

--- src/controllers/movies/addMovieController.ts
+++ src/controllers/movies/addMovieController.ts
@@ -2,48 +2,48 @@ import { Request, Response } from "express";
 import Movie, { IMovie } from "../../models/Movie";
 import { AddMovieDTO } from "./interfaces";
 import axios from "axios";
-require('dotenv').config();
+require("dotenv").config();
 
 export const addMovieController = async (req: Request, res: Response) => {
-  const contraseñaApi= process.env.API_KEY  ||""
+  const contraseñaApi = process.env.API_KEY || "";
   try {
     const newMovie: AddMovieDTO = req.body;
 
     // Traducción del título
     const titleTranslationResponse = await axios.request({
-      method: 'POST',
-      url: 'https://rapid-translate-multi-traduction.p.rapidapi.com/t',
+      method: "POST",
+      url: "https://rapid-translate-multi-traduction.p.rapidapi.com/t",
       headers: {
-        'content-type': 'application/json',
-        'X-RapidAPI-Key': contraseñaApi,
-        'X-RapidAPI-Host': 'rapid-translate-multi-traduction.p.rapidapi.com'
+        "content-type": "application/json",
+        "X-RapidAPI-Key": contraseñaApi,
+        "X-RapidAPI-Host": "rapid-translate-multi-traduction.p.rapidapi.com",
       },
       data: {
-        from: 'es',
-        to: 'en',
-        e: '',
-        q: [newMovie.titulo]
+        from: "es",
+        to: "en",
+        e: "",
+        q: [newMovie.titulo],
       },
     });
-//Cuando hacemos una solicitud a la API de traducción, obtenemos una respuesta que contiene datos. En este caso, 
-//los datos están en formato de array, donde cada elemento de la matriz corresponde a una traducción de texto.
-//con 0 accedo al primer paramatro de la matriz
+    //Cuando hacemos una solicitud a la API de traducción, obtenemos una respuesta que contiene datos. En este caso,
+    //los datos están en formato de array, donde cada elemento de la matriz corresponde a una traducción de texto.
+    //con 0 accedo al primer paramatro de la matriz
     const translatedTitle = titleTranslationResponse.data[0];
 
     // Traducción de la sinopsis
     const sinopsisTranslationResponse = await axios.request({
-      method: 'POST',
-      url: 'https://rapid-translate-multi-traduction.p.rapidapi.com/t',
+      method: "POST",
+      url: "https://rapid-translate-multi-traduction.p.rapidapi.com/t",
       headers: {
-        'content-type': 'application/json',
-        'X-RapidAPI-Key': contraseñaApi,
-        'X-RapidAPI-Host': 'rapid-translate-multi-traduction.p.rapidapi.com'
+        "content-type": "application/json",
+        "X-RapidAPI-Key": contraseñaApi,
+        "X-RapidAPI-Host": "rapid-translate-multi-traduction.p.rapidapi.com",
       },
       data: {
-        from: 'es',
-        to: 'en',
-        e: '',
-        q: [newMovie.sinopsis]
+        from: "es",
+        to: "en",
+        e: "",
+        q: [newMovie.sinopsis],
       },
     });
 
@@ -57,7 +57,7 @@ export const addMovieController = async (req: Request, res: Response) => {
     });
 
     await movie.save();
-    res.status(201).json({ message: "Película creada con éxito" , movie});
+    res.status(201).json({ message: "Película creada con éxito", movie });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Error al registrar la película" });
--- src/utils/validators/validateHelper.ts
+++ src/utils/validators/validateHelper.ts
@@ -1,11 +1,7 @@
 const { validationResult } = require("express-validator");
 import { NextFunction, Request, Response } from "express";
 
-export const ValidateResult = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+export const ValidateResult = (req: Request,res: Response,next: NextFunction) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json(errors);
