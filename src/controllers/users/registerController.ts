c5cd937794a79423586ae739ff78460491cdd13d
FlorIniguez <iniguezflorencia788@gmail.com>
2023-07-20 00:15:11 -0300

    verificaciones de username o email

--- src/controllers/users/registerController.ts
+++ src/controllers/users/registerController.ts
@@ -2,14 +2,26 @@ import { Request, Response } from "express";
 import User, { IUser } from "../../models/User";
 import { AddUserDTO } from "./interfaces";
 import bcrypt from "bcrypt";
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
 import { generateJWT } from "../../utils/middlewares/generateToken";
-dotenv.config();
+// dotenv.config();
 
 export const registerController = async (req: Request, res: Response) => {
   try {
     const newUser: AddUserDTO = req.body;
-    
+
+    // Verificar si el nombre de usuario ya está en uso
+    const existingUsernameUser: IUser | null = await User.findOne({ username: newUser.username });
+    if (existingUsernameUser) {
+      return res.status(409).json({ message: "Nombre de usuario no disponible, ya se encuentra en uso." });
+    }
+
+    // Verificar si el correo electrónico ya está en uso
+    const existingEmailUser: IUser | null = await User.findOne({ email: newUser.email });
+    if (existingEmailUser) {
+      return res.status(409).json({ message: "El correo electrónico ya está en uso." });
+    }
+
     const hashedPassword = await bcrypt.hash(newUser.password, 10);
     const user: IUser = new User({
       ...newUser,
@@ -18,7 +30,7 @@ export const registerController = async (req: Request, res: Response) => {
     const savedUser = await user.save();
     //token
     const token: string = generateJWT(savedUser._id);
-   
+
     res.header("auth-token", token).json(savedUser);
   } catch (error) {
     res
--- src/server/index.ts
+++ src/server/index.ts
@@ -4,12 +4,13 @@ const userRoutes = require('../routes/users/userRoutes')
 const moviesRoutes = require('../routes/movies/moviesRoutes')
 import swagger from '../utils/swagger';
 import handleJSONErrorMiddleware from '../utils/middlewares/validateJsonMiddleware';
-
+import cors from 'cors';
 
 const server = express();
 
 swagger('/swagger',server)
 server.use(express.json());
+server.use(cors());
 server.use(handleJSONErrorMiddleware);
 server.get('/', (req: Request,res:Response)=> {
     res.send('Welcome to my first backend project!');
